3b94dd42323b0f383ce8f58c178e48bcb0c6c1c7
Mr RaMeZaNi <115406247+Mr-RmZa@users.noreply.github.com>
2023-02-14 09:13:38 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ console.log("Hello, World!");
 
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.end("<html><body><h1>Hello, World!</h1></body></html>");
-});
-
-server.listen(3000);
+http.createServer((req, res) => {
+    res.end("Hello, World!");
+}).listen(3000);
